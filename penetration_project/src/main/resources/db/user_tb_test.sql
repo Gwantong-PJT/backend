8ffff80a6e8089dc31b21cf00749be1bdd9ed944
yaya0147 <yaya0147@gnu.ac.kr>
2024-11-06 17:07:51 +0900

    Docs : add sql query

--- penetration_project/src/main/resources/db/user_tb_test.sql
+++ penetration_project/src/main/resources/db/user_tb_test.sql
@@ -11,5 +11,15 @@ insert into users_tb(`user_id`, `user_password`, `user_name`)
 value ("bbbb","3333","test_user_1");
 
 
+select * from users_tb where user_id = "ppman";
+
+update users_tb
+set 
+user_password = "aapa",
+user_name = "hi_postman",
+user_profile = null,
+age_no = null,
+user_residence = null
+where user_id = "ppman";
 
 drop table users_tb;
